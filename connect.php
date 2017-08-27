651f5c930d277b956d7f645e61fbb2484fcd9968
OrangeJewc <samnayerman@gmail.com>
2017-08-27 15:31:56 -0700

    asdf

--- connect.php
+++ connect.php
@@ -20,7 +20,7 @@ if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
         // echo "id: " . $row["id"]. " - Name: " . $row["name"]. "<br>";
-        echo $row;
+        echo json_encode($row);
     }
 } else {
     echo "0 results";
