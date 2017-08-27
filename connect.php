3582a6f78c96df3ef3d27d28f0c52ff960919f67
OrangeJewc <samnayerman@gmail.com>
2017-08-27 15:40:44 -0700

    CORS

--- connect.php
+++ connect.php
@@ -1,31 +1,32 @@
 <!--mysql://b5ac5b817c6f25:1f694a81@us-cdbr-iron-east-05.cleardb.net/heroku_0367002a81476ef?reconnect=true-->
 <?php
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+  header("Access-Control-Allow-Origin: *");
+  $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-$server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$db = substr($url["path"], 1);
+  $server = $url["host"];
+  $username = $url["user"];
+  $password = $url["pass"];
+  $db = substr($url["path"], 1);
 
-$conn = new mysqli($server, $username, $password, $db);
+  $conn = new mysqli($server, $username, $password, $db);
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+  if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+  }
 
-$sql = "SELECT * FROM user";
-$result = $conn->query($sql);
-$rows = array();
+  $sql = "SELECT * FROM user";
+  $result = $conn->query($sql);
+  $rows = array();
 
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        // echo "id: " . $row["id"]. " - Name: " . $row["name"]. "<br>";
-      $rows['object_name'][] = $row;   
-    }
-  echo json_encode($rows);
-} else {
-    echo "0 results";
-}
-$conn->close();
+  if ($result->num_rows > 0) {
+      // output data of each row
+      while($row = $result->fetch_assoc()) {
+          // echo "id: " . $row["id"]. " - Name: " . $row["name"]. "<br>";
+        $rows['object_name'][] = $row;   
+      }
+    echo json_encode($rows);
+  } else {
+      echo "0 results";
+  }
+  $conn->close();
 ?>
