29049d82a00bf19610e7b0fcf21ca1ec5f8aed5c
OrangeJewc <samnayerman@gmail.com>
2017-08-27 16:01:06 -0700

    sdf

---
+++ addMovie.php
@@ -0,0 +1,33 @@
+<?php
+  header("Access-Control-Allow-Origin: *");
+  $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+  $server = $url["host"];
+  $username = $url["user"];
+  $password = $url["pass"];
+  $db = substr($url["path"], 1);
+
+  $conn = new mysqli($server, $username, $password, $db);
+
+  if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+  }
+
+  $test = $_GET['name'];
+  // $sql = "SELECT * FROM user";
+  // $result = $conn->query($sql);
+  // $rows = array();
+
+  // if ($result->num_rows > 0) {
+  //     // output data of each row
+  //     while($row = $result->fetch_assoc()) {
+  //         // echo "id: " . $row["id"]. " - Name: " . $row["name"]. "<br>";
+  //       $rows[] = $row;   
+  //     }
+  //   echo json_encode($rows);
+  // } else {
+  //     echo "0 results";
+  // }
+  echo $test;
+  $conn->close();
+?>
---
+++ getLists.php
@@ -0,0 +1,31 @@
+<?php
+  header("Access-Control-Allow-Origin: *");
+  $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+  $server = $url["host"];
+  $username = $url["user"];
+  $password = $url["pass"];
+  $db = substr($url["path"], 1);
+
+  $conn = new mysqli($server, $username, $password, $db);
+
+  if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+  }
+
+  $sql = "SELECT * FROM list";
+  $result = $conn->query($sql);
+  $rows = array();
+
+  if ($result->num_rows > 0) {
+      // output data of each row
+      while($row = $result->fetch_assoc()) {
+          // echo "id: " . $row["id"]. " - Name: " . $row["name"]. "<br>";
+        $rows[] = $row;   
+      }
+    echo json_encode($rows);
+  } else {
+      echo "0 results";
+  }
+  $conn->close();
+?>
