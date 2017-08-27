1e934dca61e78c07ec4ae71604d3ecf62f974275
OrangeJewc <samnayerman@gmail.com>
2017-08-27 15:27:29 -0700

    t

--- connect.php
+++ connect.php
@@ -19,7 +19,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["id"]. " - Name: " . $row["name"]. "<br>";
+        echo "id: " . $row["id"]. " - Name: " . $row["name"]. "<br>".json_encode();
     }
 } else {
     echo "0 results";
