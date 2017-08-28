c645ea458ac3097f69b5c49d109928da4590201b
OrangeJewc <samnayerman@gmail.com>
2017-08-28 15:31:18 -0700

    delete movies from lists

--- deleteList.php
+++ deleteList.php
@@ -17,11 +17,15 @@
   $id = $_GET['id'];
 
   $sql = "DELETE FROM list WHERE id = $id";
+
+  $movieSql = "DELETE FROM movie WHERE listId = $id";
   
-  if ($conn->query($sql) === TRUE) {
-    echo "Record deleted successfully";
-  } else {
+  if ($conn->query($movieSql) === TRUE) {
+    if($conn->query($sql) === TRUE) {
+      echo "Record deleted successfully";
+    } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
+    }
   }
 
   $conn->close();
