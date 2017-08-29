35fc939b7a9e81aa209bfbf64be49afb9d204299
OrangeJewc <samnayerman@gmail.com>
2017-08-28 17:36:11 -0700

    sf

--- addUser.php
+++ addUser.php
@@ -16,7 +16,7 @@
 
   $name = $_GET['name'];
 
-  $sql = "INSERT INTO user(name) VALUES('$name')";
+  $sql = "INSERT INTO user(name) OUTPUT * VALUES('$name')";
   $result = $conn->query($sql);
   
   if ($result === TRUE) {
