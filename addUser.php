0a8663c9cf39ee5b5a2b748c869429413d0d40d5
OrangeJewc <samnayerman@gmail.com>
2017-08-28 17:21:37 -0700

    addUser

---
+++ addUser.php
@@ -0,0 +1,28 @@
+<?php
+  header("Access-Control-Allow-Origin: *");
+  header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+  $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+  $server = $url["host"];
+  $username = $url["user"];
+  $password = $url["pass"];
+  $db = substr($url["path"], 1);
+
+  $conn = new mysqli($server, $username, $password, $db);
+
+  if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+  }
+
+  $name = $_GET['name'];
+
+  $sql = "INSERT INTO user(name) VALUES('$name')";
+  
+  if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+  } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+
+  $conn->close();
+?>
